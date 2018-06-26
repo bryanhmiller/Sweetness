70d26ebd492fe3b531d54f77c618a9a541acf747
bryanhmiller <bryankalos@hotmail.com>
2018-06-25 22:13:38 -0500

    Added some buttons, did not successfully install bootstrap, will probably strip it out.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './heart.svg';
 import './App.css';
+import {button} from 'react-bootstrap';
 
 class App extends Component {
   render() {
@@ -10,14 +11,21 @@ class App extends Component {
                         "text-shadow": "5px 5px 5px #7F0C15" 
                       }
     let headerImage = {
-                      }                    
+                      }
+                        
     return (
       <div className="App">
         <header className="App-header" style={headerStyle}>
           <img src={logo} className="App-logo" alt="logo" style={headerImage} />
-          <h1 className="App-title">Welcome to Sweetness</h1>
+          <h3 className="App-welcome">Welcome to</h3>
+          <h1 className="App-Name">Sweetness</h1>
         </header>
-        <p className="App-intro">Songs</p>
+        <button className="App-songs" >Songs</button>
+        <button className="App-cuteness">Cuteness</button>
+        <button className="App-poems">Poems</button>
+        <button className="App-verses">Verses</button>
+        <button className="App-surprises">Surprises</button>
+        <button className="App-messages">Messages</button>
       </div>
     );
   }
